d74e547682cfc45d36cd3b033e011d5a2dda0ff4
Alex Anderson <alexanderson1993@gmail.com>
2020-12-14 14:11:14 -0500

    Add TypeScript Configuration.

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,32 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const CopyPlugin = require('copy-webpack-plugin')
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  entry: './app/index.js',
+  entry: "./app/index.tsx",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'index_bundle.js',
-    publicPath: '/'
+    path: path.resolve(__dirname, "dist"),
+    filename: "index_bundle.js",
+    publicPath: "/",
+  },
+  resolve: {
+    // Add `.ts` and `.tsx` as a resolvable extension.
+    extensions: [".ts", ".tsx", ".js"],
   },
   module: {
     rules: [
-      { test: /\.(js)$/, use: 'babel-loader' },
-      { test: /\.css$/, use: [ 'style-loader', 'css-loader' ]}
-    ]
+      { test: /\.(js|ts|tsx)$/, use: "babel-loader" },
+      { test: /\.css$/, use: ["style-loader", "css-loader"] },
+    ],
   },
-  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+  mode: process.env.NODE_ENV === "production" ? "production" : "development",
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'app/index.html'
+      template: "app/index.html",
     }),
-    new CopyPlugin([
-      { from : '_redirects' }
-    ])
+    new CopyPlugin([{ from: "_redirects" }]),
   ],
   devServer: {
-    historyApiFallback: true
-  }
-}
+    historyApiFallback: true,
+  },
+};
